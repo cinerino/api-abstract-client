258f9a39f369fdfb8f636cd0906c129e0379806f
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2018-10-03 11:28:01 +0000

    ### Changed - 上映イベント予約検索を追加。

--- src/service/reservation.ts
+++ src/service/reservation.ts
@@ -3,12 +3,34 @@ import { OK } from 'http-status';
 import * as factory from '../factory';
 import { Service } from '../service';
 
-export type IScreeningEventReservationOwnershipInfo =
-    factory.ownershipInfo.IOwnershipInfo<factory.chevre.reservation.event.IReservation<factory.chevre.event.screeningEvent.IEvent>>;
+export type IScreeningEvent = factory.chevre.event.screeningEvent.IEvent;
+export type IScreeningEventReservation = factory.chevre.reservation.event.IReservation<IScreeningEvent>;
+export type IScreeningEventReservationOwnershipInfo = factory.ownershipInfo.IOwnershipInfo<IScreeningEventReservation>;
 /**
  * 予約サービス
  */
 export class ReservationService extends Service {
+    /**
+     * 上映イベント予約検索
+     */
+    public async searchScreeningEventReservations(
+        params: factory.chevre.reservation.event.ISearchConditions
+    ): Promise<{
+        totalCount: number;
+        data: IScreeningEventReservation[];
+    }> {
+        return this.fetch({
+            uri: '/reservations/eventReservation/screeningEvent',
+            method: 'GET',
+            qs: params,
+            expectedStatusCodes: [OK]
+        }).then(async (response) => {
+            return {
+                totalCount: Number(<string>response.headers.get('X-Total-Count')),
+                data: await response.json()
+            };
+        });
+    }
     /**
      * トークンで上映イベント予約照会
      */
