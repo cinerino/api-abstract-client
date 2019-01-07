c6a8c685a4f166654ccc09baec5645fffc98d7ea
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2019-01-07 06:56:32 +0000

    # CINERINO-42 ### Changed - 座席予約承認時に決済方法を指定できるように調整

--- src/service/transaction/placeOrder.ts
+++ src/service/transaction/placeOrder.ts
@@ -52,9 +52,9 @@ export class PlaceOrderTransactionService extends Service implements Transaction
      * 座席予約承認作成
      */
     public async authorizeSeatReservation(params: {
-        object: factory.chevre.transaction.reserve.IObjectWithoutDetail;
+        object: factory.action.authorize.offer.seatReservation.IObjectWithoutDetail;
         purpose: factory.action.authorize.offer.seatReservation.IPurpose;
-    }): Promise<factory.action.authorize.offer.seatReservation.IAction> {
+    }): Promise<factory.action.authorize.offer.seatReservation.IAction<factory.service.webAPI.Identifier>> {
         return this.fetch({
             uri: `/transactions/${this.typeOf}/${params.purpose.id}/actions/authorize/offer/seatReservation`,
             method: 'POST',
