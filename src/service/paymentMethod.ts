7993eb5f5e9f063af3e90325d2d75099748eadf2
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2020-05-02 07:37:00 +0000

    ### Added - プリペイドカード作成を追加 - プリペイドカード検索を追加

--- src/service/paymentMethod.ts
+++ src/service/paymentMethod.ts
@@ -1,6 +1,7 @@
-import { OK } from 'http-status';
+import { CREATED, OK } from 'http-status';
 
 import * as factory from '../factory';
+
 import { ISearchResult, Service } from '../service';
 
 /**
@@ -14,7 +15,44 @@ export class PaymentMethodService extends Service {
         params: factory.paymentMethod.ISearchConditions<factory.paymentMethodType.MovieTicket>
     ): Promise<ISearchResult<factory.paymentMethod.paymentCard.movieTicket.IMovieTicket[]>> {
         return this.fetch({
-            uri: '/paymentMethods/movieTicket',
+            uri: `/paymentMethods/${factory.paymentMethodType.MovieTicket}`,
+            method: 'GET',
+            qs: params,
+            expectedStatusCodes: [OK]
+        })
+            .then(async (response) => {
+                return {
+                    totalCount: (typeof response.headers.get('X-Total-Count') === 'string')
+                        ? Number(<string>response.headers.get('X-Total-Count'))
+                        : undefined,
+                    data: await response.json()
+                };
+            });
+    }
+
+    /**
+     * プリペイドカード作成
+     */
+    public async createPrepaidCard(
+        params: factory.paymentMethod.paymentCard.prepaidCard.IPrepaidCard
+    ): Promise<factory.paymentMethod.paymentCard.prepaidCard.IPrepaidCard> {
+        return this.fetch({
+            uri: `/paymentMethods/${factory.paymentMethodType.PrepaidCard}`,
+            method: 'POST',
+            body: params,
+            expectedStatusCodes: [CREATED]
+        })
+            .then(async (response) => response.json());
+    }
+
+    /**
+     * プリペイドカード検索
+     */
+    public async searchPrepaidCards(
+        params: any
+    ): Promise<ISearchResult<factory.paymentMethod.paymentCard.prepaidCard.IPrepaidCard[]>> {
+        return this.fetch({
+            uri: `/paymentMethods/${factory.paymentMethodType.PrepaidCard}`,
             method: 'GET',
             qs: params,
             expectedStatusCodes: [OK]
