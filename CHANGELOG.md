9ed93cad1616b8d8a4ee365fda006457ef9d9975
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2020-07-06 05:14:35 +0000

    ### Changed - ポイントインセンティブ承認からnotesの指定を削除

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -10,6 +10,8 @@ The format is based on [Keep a Changelog](http://keepachangelog.com/).
 
 ### Changed
 
+- ポイントインセンティブ承認からnotesの指定を削除
+
 ### Deprecated
 
 ### Removed
--- src/service/transaction/placeOrder.ts
+++ src/service/transaction/placeOrder.ts
@@ -86,13 +86,7 @@ export class PlaceOrderTransactionService extends Service implements Transaction
      * ポイントインセンティブ承認
      */
     public async authorizePointAward(params: {
-        object: {
-            /**
-             * 説明
-             * 指定すると、口座の取引明細に記録されます。
-             */
-            notes?: string;
-        };
+        object: {};
         purpose: factory.action.authorize.award.point.IPurpose;
     }): Promise<factory.action.authorize.award.point.IAction> {
         return this.fetch({
